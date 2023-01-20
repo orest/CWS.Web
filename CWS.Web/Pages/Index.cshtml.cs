e932d7c3b3399096c4f70ea5bd98e99219f8cf98
Orest VM <orest.khrapko@cardworks.com>
2023-01-20 17:49:13 -0500

    add plastic id

--- CWS.Web/Pages/Index.cshtml.cs
+++ CWS.Web/Pages/Index.cshtml.cs
@@ -16,7 +16,7 @@ namespace CWS.Web.Pages
         public void OnGet()
         {
             var account = new CWS.DemoLib1.Account();
-            var platic = new DemoLib1.Plastic();
+            var platic = new DemoLib1.Plastic() { PlasticId = 1 };
         }
     }
 }
